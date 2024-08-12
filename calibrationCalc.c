384eaed3eb6ca13147b8c33b0a5e913656fa0821
Joseph Medernach <mr.rivertimmothyotter@gmail.com>
2024-08-11 18:37:07 -0600

    Find First Digit including Words

--- calibrationCalc.c
+++ calibrationCalc.c
@@ -1,34 +1,59 @@
 /* J.Med, mr.rivertimmothyotter@gmail.com */
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
 
 #define LINE_LEN 64
+#define NUM_DIGS 9
 
 
 
 int main() {
-    int calib_sum;
-    size_t i;
-    char line[64];
-    calib_sum = 0;
+    int calib_sum, add, i;
+    char line[LINE_LEN], *first, *tmp;
+    char *digs[] = {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    };
+
+    calib_sum=0;
 
     while (fgets(line, LINE_LEN, stdin) != NULL) {
-        /* get first digit from line */
-        i = 0;
-        while (i < LINE_LEN && line[i] != '\n' && line[i] != '\0' &&
-                (line[i] > 58 || line[i] < 47)) {
-            i++; 
+        /* Get the first digit */
+        first = strpbrk(line, "123456789");
+        for (i = 0; i < NUM_DIGS; i++) {
+            if (first == NULL) first = strstr(line, digs[i]);
+            else if ((tmp = strstr(line, digs[i])) != NULL && tmp < first)
+                first = tmp;
         }
-        calib_sum = calib_sum + ((int) line[i] - 48) * 10;
-
-        /* get last digit from line */
-        i = strlen(line);
-        while (i>0 && (line[i] > 58 || line[i] < 47)) {
-            i--;
+        if (*first <= 57 && *first >= 48) add = ((int) *first - 48) * 10;
+        else switch (*first) {
+            case 'o':   add = 10;
+                        break;
+            case 't':   if (first[1] == 'w') add = 20;
+                        else add = 30;
+                        break;
+            case 'f':   if (first[1] == 'o') add = 40;
+                        else add = 50;
+                        break;
+            case 's':   if (first[1] == 'i') add = 60;
+                        else add = 70;
+                        break;
+            case 'e':   add = 80;
+                        break;
+            case 'n':   add = 90;
+                        break;
+            default:    printf("Err: default switch statement finding first");
+                        return 1;
         }
-        calib_sum = calib_sum + ((int) line[i] - 48);
+        calib_sum += add;
     }
 
     if (printf("%d\n", calib_sum) < 0) {
